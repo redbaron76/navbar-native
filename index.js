8ff4f686342265ddc0b34e03931fa986a36dee97
redbaron76 <f.fumis@gmail.com>
2016-09-20 10:58:55 +0200

    exporting utility functions

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Container from './components/container'
 import Navbar from './components/navbar';
+import { iOS, isIOS } from './utils';
 
 module.exports = {
     Container: Container,
     Navbar: Navbar,
+    iOS: iOS,
+    isIOS: isIOS,
 };
