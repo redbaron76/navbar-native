b962cb7596390e5f455cffb3912184f8295922e0
Guillaume Hain <zedtux@zedroot.org>
2018-07-06 15:30:17 +0200

    Fixes all library issues * Fixes very old versions of the dependencies * Migrated to the prop-types package

--- components/container.js
+++ components/container.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { ListView, ScrollView, View, Text, Dimensions } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import Spinner from 'react-native-spinkit';
